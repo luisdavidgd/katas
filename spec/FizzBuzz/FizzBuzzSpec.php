c9be0f31dc2c92ecb917c31e5f8ed4dcdd952823
Luis David <luisdavidgd@gmail.com>
2019-08-18 16:23:10 -0500

    FizzBuzz Kata Added

--- spec/FizzBuzz/FizzBuzzSpec.php
+++ spec/FizzBuzz/FizzBuzzSpec.php
@@ -2,14 +2,52 @@
 
 namespace spec\FizzBuzz;
 
-use FizzBuzz\FizzBuzz;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 
 class FizzBuzzSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_translates_1_for_fizzbuzz()
     {
-        $this->shouldHaveType(FizzBuzz::class);
+        $this->execute(1)->shouldReturn(1);
+    }
+
+    public function it_translates_2_for_fizzbuzz()
+    {
+        $this->execute(2)->shouldReturn(2);
+    }
+
+    public function it_translates_3_for_fizzbuzz()
+    {
+        $this->execute(3)->shouldReturn('fizz');
+    }
+
+    public function it_translates_5_for_fizzbuzz()
+    {
+        $this->execute(5)->shouldReturn('buzz');
+    }
+
+    public function it_translates_6_for_fizzbuzz()
+    {
+        $this->execute(6)->shouldReturn('fizz');
+    }
+
+    public function it_translates_10_for_fizzbuzz()
+    {
+        $this->execute(10)->shouldReturn('buzz');
+    }
+
+    public function it_translates_15_for_fizzbuzz()
+    {
+        $this->execute(15)->shouldReturn('fizzbuzz');
+    }
+
+    public function it_translates_123_for_fizzbuzz()
+    {
+        $this->execute(123)->shouldReturn('fizz');
+    }
+
+    public function it_translates_a_sequence_of_number_for_fizzbuzz()
+    {
+        $this->executeUpTo(10)->shouldReturn([1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz']);
     }
 }
