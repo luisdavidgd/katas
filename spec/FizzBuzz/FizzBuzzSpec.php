1556de94aefc2ee740c341963d619ea67f56f2b7
Luis David <luisdavidgd@gmail.com>
2019-08-18 16:05:55 -0500

    Update readme files

---
+++ spec/FizzBuzz/FizzBuzzSpec.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace spec\FizzBuzz;
+
+use FizzBuzz\FizzBuzz;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class FizzBuzzSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(FizzBuzz::class);
+    }
+}
---
+++ src/FizzBuzz/FizzBuzz.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace FizzBuzz;
+
+class FizzBuzz
+{
+}
